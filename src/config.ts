cd50521039ce2a3af5991d54ef44e9ba60bab713
Vũ Thành Trung <69748032+vuthanhtrung2010@users.noreply.github.com>
2024-04-21 08:00:38 +0700

    Initial commit

---
+++ src/config.ts
@@ -0,0 +1,3 @@
+export const PREFIX: Array<string> = ["Bot Prefix"];
+export const BOT_TOKEN: string = "Bot Token";
+export const OWNER_IDS: Array<string> = ["Bot Owner Discord ID"];
---
+++ src/structures/commandOptions/globalCooldown.ts
@@ -0,0 +1,30 @@
+import { EmbedBuilder, Interaction, Message, DiscordClient } from "discord.js";
+import { AnyCommand, InteractionTypeOptions } from "../../types.js";
+
+export const globalCooldownFN = async(client: DiscordClient, message: Message | Interaction<"cached">, command: AnyCommand, interactionType: InteractionTypeOptions): Promise<boolean> => {
+    if (!command.globalCooldown || isNaN(command.globalCooldown)) return true;
+
+    const dbData = `globalCooldown.${interactionType}.${command.name}.${message.member?.id}`
+    const currentTime: number = Date.now();
+    const storedTime: number = client.cooldownDB?.get(dbData) ?? 0;
+
+    if (Math.floor(currentTime - storedTime) >= command.globalCooldown || !storedTime) {
+        client.cooldownDB?.set(dbData, currentTime);
+        return true;
+    } else {
+        if (command.returnErrors === false || command.returnGlobalCooldownError === false) return false;
+        message.channel?.send({
+            embeds: [new EmbedBuilder()
+                .setColor("DarkRed")
+                .setTimestamp()
+                .setAuthor({
+                    name: message.member?.user.globalName ?? message.member?.user.username ?? "",
+                    iconURL: message.member?.user.displayAvatarURL()
+                })
+                .setThumbnail(client.user.displayAvatarURL())
+                .setDescription(`You are currently at cooldown. Please try again in <t:${Math.floor(Math.floor(storedTime + command.globalCooldown) / 1000)}:R>.`)
+            ],
+        });
+        return false;
+    }
+};
